49d150f50a7f170319521039a5b87785a1c7c9b9
DurgaPutta <154326446+DurgaPutta@users.noreply.github.com>
2024-12-31 10:19:45 +0530

    Add files via upload
    Activity 7 and Activity 8 completed and added

---
+++ SQL/Activities/Activity7.sql
@@ -0,0 +1,7 @@
+---------Activity 7---------
+SELECt * from Orders
+select SUM(PURCHASE_AMOUNT) from Orders;
+select AVG(PURCHASE_AMOUNT) from Orders;
+select Max(PURCHASE_AMOUNT) from Orders;
+select Min(PURCHASE_AMOUNT) from Orders;
+select AVG(PURCHASE_AMOUNT) from Orders;
